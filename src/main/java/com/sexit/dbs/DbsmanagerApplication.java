f55e5b0b92eb2acb251e548037a7123d50687c47
SexitLiu <951615189@qq.com>
2019-03-14 16:32:13 +0800

    springboot config

--- src/main/java/com/sexit/dbs/DbsmanagerApplication.java
+++ src/main/java/com/sexit/dbs/DbsmanagerApplication.java
@@ -1,9 +1,13 @@
-package com.sexit.dbs.dbsmanager;
+package com.sexit.dbs;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@MapperScan("com.sexit.dbs.dao") // 配置mybatis-dao层扫描
+@EnableTransactionManagement
 public class DbsmanagerApplication {
 
     public static void main(String[] args) {
