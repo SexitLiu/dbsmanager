a2765730d6f269c7677bdfcba006212dd8b2f9a5
SexitLiu <951615189@qq.com>
2019-03-14 15:45:40 +0800

    springboot run_application

---
+++ src/main/java/com/sexit/dbs/dbsmanager/DbsmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.sexit.dbs.dbsmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbsmanagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbsmanagerApplication.class, args);
+    }
+
+}
